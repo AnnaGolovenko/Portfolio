23b765238480bfdaf70bd58998f9eac4b4af440e
Madfinn <madfinn@outlook.com>
2018-01-27 11:48:14 +0200

    rewrite lp

--- 3 Alexis/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
- $('.menu').find('a').on('click', function(e){
-  e.preventDefault();
-     var id  = $(this).attr('href'), top = $(id).offset().top;
-     $('body,html').animate({scrollTop: top}, 1000);
- })
-});
